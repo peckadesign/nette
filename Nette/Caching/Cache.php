0a298c8e7fbde7b9c9884b60e22239f1974a0e46
Vít Kutný <vitkutny@icloud.com>
2015-06-16 10:39:30 +0200

    Cache::load() fixed receiving dependencies as reference - https://github.com/nette/caching/commit/6093d0fc2f647315a38f3a48de32caa997b5c4f7

--- Nette/Caching/Cache.php
+++ Nette/Caching/Cache.php
@@ -98,7 +98,10 @@ class Cache extends Nette\Object implements \ArrayAccess
 	{
 		$data = $this->storage->read($this->generateKey($key));
 		if ($data === NULL && $fallback) {
-			return $this->save($key, Callback::closure($fallback));
+			//https://github.com/nette/caching/commit/6093d0fc2f647315a38f3a48de32caa997b5c4f7
+			return $this->save($key, function (& $dependencies) use ($fallback) {
+				return call_user_func_array($fallback, array(& $dependencies));
+			});
 		}
 		return $data;
 	}
